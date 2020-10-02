bd7e4c76b4260bceb00f9b68ba117c78f25469df
Jose Papitha <38282455+JosePapitha@users.noreply.github.com>
2020-10-02 07:47:34 -0400

    Add files via upload
    Commit Basic code snippets

---
+++ Codes/Basics.py
@@ -0,0 +1,41 @@
+#variables and types
+myint = 1
+print(myint)
+
+myfloat = 0.7
+mynewfloat = float(7)
+print(myfloat)
+print(mynewfloat)
+
+#single '' can be used but may terminate the string if it has apostropes
+print('jose')
+
+#operators
+var1 = 5
+var2 = 10
+add = var1 + var2
+print(add)
+
+str1 = "hello"
+str2 = "world"
+result = str1 + " " +str2
+print(result)
+
+#IF loop
+x=1
+if x == 1:
+	print("x is true")
+
+#Assignment operators
+a,b = 5,1
+print(a,b)
+
+#isinstance -- like boolean returning true or false
+myintvalue = 7
+if isinstance(myintvalue,float) :
+	print("Integer is : %d" %myintvalue)
+else:
+	print("It is not a Integer")
+
+
+
